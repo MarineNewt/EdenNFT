88b945713ffc7be6bfcdd6d1123931f7712df820
Frankie Leyva <frankieiii15@comcast.net>
2023-02-23 18:09:44 -0800

    Add info Sections

--- src/components/infoPage.js
+++ src/components/infoPage.js
@@ -7,8 +7,20 @@ class InfoPage extends Component {
     return (
       <div className=" ">
           <div className='compbox comp-font-sizer centerit' style={{width: 'calc(50vw + 120px)'}}>
-            <h1 className="title-border" style={{width: 'calc(11vw + 225px)'}}>Mint a Flower</h1>
-            <p> info Start</p>
+            <h1 className="title-border" style={{width: 'calc(11vw + 225px)'}}>About Eden's Flowers</h1>
+
+            <p>Eden's Flowers is a collection of 100 unique Flowers with dyanamic interactions, both interactive and automatic, built as a second layer to the Love Token. The initial supply of Flowers was airdropped to Love Token holders on February 19th 2023, with the remaining supply being available for purchase. The collection aims to emphasize human experiences of connection, equality, and impermenance through imagery and mechanics. The most prominent aspect of the collection is the seperation of blooming revealed flowers, and dead unrevealed flowers. "Without Love there is no chance to bloom." Flower holders who do not own a Love Token will only be able to own Dead Flowers. Dead Flowers may be transferred and sold as normal, but may not partkae the collection mechanism and will be represented by a single image of a oppurtunistic pile of dirt. Upon the owner of a dead flower recieving a Love Token their flower will then be revealed and again be eligible for the mechanism. Flowers can switch back and forth between the states of Dead and Blooming as they move wallets, depending whether their current owner is a Love token holder. This collection is both a gift to Love token holders who represent my early community and a artistic expression of code manifesting itself as a interactive art piece.</p>
+
+            <h2>Love Token</h2>
+            <p></p>
+            <h2>The Garden</h2>
+            <p></p>
+            <h2>Switching flowers</h2>
+            <p></p>
+            <h2>The End</h2>
+            <p></p>
+            <h2>The Creator - Newt9</h2>
+            <p></p>
           </div>
       </div>
     );
